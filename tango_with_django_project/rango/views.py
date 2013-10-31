e8e5056274b353243c4ad2556f6fdcab7fa5d17d
Kentoseth <me@kentoseth.com>
2013-10-31 02:04:49 +0200

    Added an about page. Can't seem to add links yet though

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Rango says hello world!")
+	
+def about_page(request):
+	return HttpResponse("Rango says this is the about page")
